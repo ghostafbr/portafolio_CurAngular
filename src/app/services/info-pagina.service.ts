c88fcb58a0f6f24bffd0472880c637ca411b37bf
Andres Bolaños <ghostafbr@gmail.com>
2019-07-04 21:39:59 -0500

    Fin de la sección 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -7,23 +7,35 @@ import { InfoPagina } from '../interfaces/info-pagina.interfaces';
   providedIn: 'root'
 })
 export class InfoPaginaService {
-
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any = [];
+
   constructor( private http: HttpClient) {
 
-    // Leer archivo JSON
-    this.http.get('assets/data/data-pagina.json')
-    .subscribe( (resp: InfoPagina) => {
-
-      this.cargada = true;
-      this.info = resp;
-
-      console.log( resp );
-
-    }      );
-
+    this.cargarInfo();
+    this.cargarEquipo();
 
    }
+
+private cargarInfo (){
+
+ // Leer archivo JSON
+ this.http.get('assets/data/data-pagina.json')
+ .subscribe( (resp: InfoPagina) => {
+   this.cargada = true;
+   this.info = resp;
+ });
 }
+
+private cargarEquipo() {
+
+  // Leer archivo JSON
+ this.http.get('https://curso-angular-15f64.firebaseio.com/equipo.json')
+ .subscribe( (resp: any []) => {
+
+   this.equipo = resp;
+ });
+
+}}
