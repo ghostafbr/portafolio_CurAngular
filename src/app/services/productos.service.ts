511fb6534d9f65c34998d7fa4d3e754ea13019fb
Andres Bolaños <ghostafbr@gmail.com>
2019-07-05 22:05:43 -0500

    listo para producción

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interface';
-import { resolve } from 'dns';
-import { reject } from 'q';
+
 
 
 @Injectable({
@@ -45,7 +44,7 @@ export class ProductosService {
   buscarProducto(termino: string) {
 
 
-    if (this.productos.length == 0) {
+    if (this.productos.length === 0) {
       // cargar productos
       this.cargarProductos().then(() => {
         // ejecutar despues de tener los productos
